34e66a20b0391dc22be82e02c562fd7ee4b26636
pranjal161 <161pranjal@gmail.com>
2023-02-13 14:05:35 +0000

    Reset & Disable call button

--- src/App.js
+++ src/App.js
@@ -8,8 +8,10 @@ const App = () => {
   const [from, updateFrom] = useState();
   const [message, updatemsg] = useState();
   const [to, updateTo] = useState();
+  const [disableCallBtn, setDisable] = useState(false);
 
   const call = () => {
+    setDisable(true);
     fetch('https://fo82xa5uv0.execute-api.eu-west-1.amazonaws.com/dev/call', {
       method: 'POST',
       body: JSON.stringify({ from: from, to: to, message: message }),
@@ -25,15 +27,26 @@ const App = () => {
     });
   }
 
+  const reset = () => {
+    updateFrom('');
+    updateTo('');
+    updatemsg('');
+    setDisable(false);
+  }
+
   return (
     <div className="App">
       <Header />
       <div style={{ marginTop: '2%' }}>
-        <input style={{ marginRight: '1%' }} onChange={(event) => updateFrom(event.target.value)} placeholder='From'></input>
-        <input style={{ marginRight: '1%' }} onChange={(event) => updateTo(event.target.value)} placeholder='To'></input>
-        <input style={{ marginRight: '1%' }} onChange={(event) => updatemsg(event.target.value)} placeholder='Message'></input>
-        <button onClick={() => call()}>Call</button>
+        <input style={{ marginRight: '1%' }} value={from} onChange={(event) => updateFrom(event.target.value)} placeholder='From'></input>
+        <input style={{ marginRight: '1%' }} value={to} onChange={(event) => updateTo(event.target.value)} placeholder='To'></input>
+        <input style={{ marginRight: '1%' }} value={message} onChange={(event) => updatemsg(event.target.value)} placeholder='Message'></input>
+        <button style={{ marginRight: '1%' }} disabled={disableCallBtn} onClick={() => call()}>Call</button>
+        <button onClick={() => reset()}>Reset</button>
       </div>
+      {disableCallBtn && <div>
+        Calling...
+      </div>}
     </div>
   );
 }
